c1a8f9920ef1f0c8c7e56666725edf6d9441d7b1
Steven Tseng <booda406@gmail.com>
2013-10-29 15:34:23 +0800

    after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,16 @@ class ApplicationController < ActionController::Base
 	DeviseController.respond_to :html, :json
 	protect_from_forgery with: :exception
 
+	def after_sign_in_path_for(resource)
+      if resource.kind_of? User
+        root_path
+      else resource.kind_of? Admin
+        check_path
+      end     
+	end
+
+  def after_sign_up_path_for(resource)
+    new_seller_session_path
+  end
+
 end
