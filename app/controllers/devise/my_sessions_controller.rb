73ae51a8eb6fb73d7789f49a70c75838918e9bbc
Steven Tseng <booda406@gmail.com>
2013-10-31 00:54:33 +0800

    add     skip_before_filter :verify_authenticity_token

--- app/controllers/devise/my_sessions_controller.rb
+++ app/controllers/devise/my_sessions_controller.rb
@@ -1,5 +1,6 @@
 class Devise::MySessionsController < Devise::SessionsController
   before_filter :authenticate_user!, :except => [:create, :destroy]
+  skip_before_filter :verify_authenticity_token, :only => [:create, :destroy]
   respond_to :json
 
   def create
