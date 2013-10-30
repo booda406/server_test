7fda0091c4efd8f2f9bc5da9ace1794b4050736d
Steven Tseng <booda406@gmail.com>
2013-10-30 22:26:11 +0800

    add authentication

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131024101919) do
+ActiveRecord::Schema.define(version: 20131030135354) do
 
   create_table "images", force: true do |t|
     t.string   "image"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 20131024101919) do
     t.string   "title"
     t.text     "body"
     t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "products", force: true do |t|
+    t.string   "name"
+    t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -41,6 +48,7 @@ ActiveRecord::Schema.define(version: 20131024101919) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "authentication_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
