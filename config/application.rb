d64318fe05c8814fe510e8143cb7d43a9618e289
Steven Tseng <booda406@gmail.com>
2013-11-05 16:10:23 +0800

    chg null session

--- config/application.rb
+++ config/application.rb
@@ -19,13 +19,5 @@ module ServerTest
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    def verified_request?
-       if request.content_type == "application/json"
-          super()
-        else
-            true
-        end
-    end
-
   end
 end
