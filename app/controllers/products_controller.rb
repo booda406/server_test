5b94e30a7fdb290540e9f64cefb8114158cae0d8
Steven Tseng <booda406@gmail.com>
2013-11-06 14:08:12 +0800

    restore product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -27,31 +27,6 @@ class ProductsController < ApplicationController
   def create
     @product = Product.new(product_params)
 
-  respond_to do |format|
-      format.json {
- #create a new image so that you can call it's class method (a bit hacky, i know)
-        @product = Image.new
-#get the json image data
-        pixels = params[:image]
-#convert it from hex to binary
-      pixels = @product.hex_to_string(pixels)
-#create it as a file
-        data = StringIO.new(pixels)
-#set file types
-        data.class.class_eval { attr_accessor :original_filename, :content_type }
-        data.original_filename = "test1.jpeg"
-        data.content_type = "image/jpeg"
-#set the image id, had some weird behavior when i didn't
-        @product.id = Image.count + 1
-#upload the data to Amazon S3
-       # @image.upload(data)
-#save the image
-        if @product.save!
-          render :nothing => true
-        end
-      }
-    end
-
     respond_to do |format|
       if @product.save
         format.html { redirect_to @product, notice: 'Product was successfully created.' }
@@ -97,18 +72,4 @@ class ProductsController < ApplicationController
     def product_params
       params.require(:product).permit(:name, :description, :image)
     end
-    #stores the file
-  def upload(file)
-    self.path.store!(file)
-  end
-  
-#converts the data from hex to a string -> found code here http://4thmouse.com/index.php/2008/02/18/converting-hex-to-binary-in-4-languages/
-  def hex_to_string(hex)
-  temp = hex.gsub("\s", "");
-    ret = []
-    (0...temp.size()/2).each{|index| ret[index] = [temp[index*2, 2]].pack("H2")}
-    file = String.new
-    ret.each { |x| file << x}
-    file  
-  end
 end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -16,20 +16,6 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-#stores the file
-  def upload(file)
-    self.path.store!(file)
-  end
-  
-#converts the data from hex to a string -> found code here http://4thmouse.com/index.php/2008/02/18/converting-hex-to-binary-in-4-languages/
-  def hex_to_string(hex)
-  temp = hex.gsub("\s", "");
-    ret = []
-    (0...temp.size()/2).each{|index| ret[index] = [temp[index*2, 2]].pack("H2")}
-    file = String.new
-    ret.each { |x| file << x}
-    file  
-  end
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
