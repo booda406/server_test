217bf859c330bc8ad91a1c32ef52f311186cc10a
Steven Tseng <booda406@gmail.com>
2013-11-06 14:03:01 +0800

    def upload

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -97,4 +97,18 @@ class ProductsController < ApplicationController
     def product_params
       params.require(:product).permit(:name, :description, :image)
     end
+    #stores the file
+  def upload(file)
+    self.path.store!(file)
+  end
+  
+#converts the data from hex to a string -> found code here http://4thmouse.com/index.php/2008/02/18/converting-hex-to-binary-in-4-languages/
+  def hex_to_string(hex)
+  temp = hex.gsub("\s", "");
+    ret = []
+    (0...temp.size()/2).each{|index| ret[index] = [temp[index*2, 2]].pack("H2")}
+    file = String.new
+    ret.each { |x| file << x}
+    file  
+  end
 end
