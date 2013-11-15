0b2e54539f78a367dedc6360a9f99f743654b25b
Steven Tseng <booda406@gmail.com>
2013-11-15 13:25:13 +0800

    chg products create and model for json

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,10 @@
 class Product < ActiveRecord::Base
 	mount_uploader :image, ImageUploader
+
+ before_save :update_images_attributes
+
+  def update_images_attributes
+      self.file_size = image.file.size
+  end
+
 end
