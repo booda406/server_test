d57b9c503c0ee6e2de3dae7c475046601e3cb469
Steven Tseng <booda406@gmail.com>
2014-04-17 16:08:22 +0800

    fix create

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -25,30 +25,34 @@ class ProductsController < ApplicationController
   # POST /products
   # POST /products.json
   def create
-    # if request.format.json?
-    #       #check if file is within picture_path
-    #   if params[:product][:image]["file"]
-    #        picture_path_params = params[:product][:image]
-    #        #create a new tempfile named fileupload
-    #        tempfile = Tempfile.new("fileupload")
-    #        tempfile.binmode
-    #        #get the file and decode it with base64 then write it to the tempfile
-    #        tempfile.write(Base64.decode64(picture_path_params["file"]))
+
+
+    if request.format.json?
+          #check if file is within picture_path
+      if params[:image]["file"]
+           picture_path_params = params[:image]
+           #create a new tempfile named fileupload
+           tempfile = Tempfile.new("fileupload")
+           # tempfile.binmode
+           #get the file and decode it with base64 then write it to the tempfile
+           # tempfile.write(Base64.decode64(picture_path_params["file"]))
      
-    #        #create a new uploaded file
-    #        uploaded_file = ActionDispatch::Http::UploadedFile.new(:tempfile => tempfile, :filename => picture_path_params["filename"], :original_filename => picture_path_params["original_filename"]) 
+           #create a new uploaded file
+           uploaded_file = ActionDispatch::Http::UploadedFile.new(:tempfile => tempfile, :filename => picture_path_params["filename"], :original_filename => picture_path_params["original_filename"]) 
      
-    #        #replace picture_path with the new uploaded file
-    #        params[:product][:image] =  uploaded_file
+           #replace picture_path with the new uploaded file
+           params[:image] =  uploaded_file
      
-    #   end
-    #   @product = Product.new.permit!
-    # end
+      end
+      @product = Product.new.permit!
+    end
+    
+    @product = Product.new(product_params)
+    
     @image = params[:image_field]
 
-    @product = Product.new
     @product.image = @image
-
+    
     respond_to do |format|
       if @product.save
         format.html { redirect_to @product, notice: 'Product was successfully created.' }
